46f845fe1b8a086faaecec5a86016f3f9079a5f6
Elias Vincent Peters-El Idrissi <eliasvpeters@gmail.com>
2023-07-31 00:56:58 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# README
+
+go project with solutions for the [GRIND 75](https://www.techinterviewhandbook.org/grind75) questions.
+
+## Goal
+
+The Goal is to solve all questions in 8 Weeks.
+
+## Progress
+
+Week: 1
+Completed: *0* / 75
---
+++ src/week_1/two_sum_test.go
@@ -0,0 +1,43 @@
+package week1
+
+import (
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+type test_data struct {
+	nums   []int
+	target int
+	want   []int
+}
+
+func TestSolve(t *testing.T) {
+	for index, data := range data() {
+		result := Solve(data.nums, data.target)
+		if !cmp.Equal(data.want, result) {
+			t.Fatalf(`Test #%d failed. Expected "%v", but got "%v"`, index, data.want, result)
+		}
+	}
+}
+
+func data() []test_data {
+	return []test_data{
+		test_data{
+			nums:   []int{2,7,11,15},
+			target: 9,
+			want:   []int{0,1},
+		},
+		test_data{
+			nums:   []int{3,2,4},
+			target: 6,
+			want:   []int{1, 2},
+		},
+		test_data{
+			nums:   []int{3, 3},
+			target: 3,
+			want:   []int{1, 2},
+		},
+	}
+
+}
